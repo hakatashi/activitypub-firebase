109906e57028c6120be30a9f890977ce5e7241f3
Koki Takahashi <hakatasiloving@gmail.com>
2023-07-26 14:34:32 +0000

    Add tests for /.well-known

--- functions/test/integration/index.spec.ts
+++ functions/test/integration/index.spec.ts
@@ -1,9 +1,11 @@
-import {describe, expect, test, afterEach, jest} from '@jest/globals';
+import {describe, expect, test, afterEach, jest, beforeEach} from '@jest/globals';
 import request from 'supertest';
 import {activitypub, apex} from '../../src/activitypub.js';
 
 const firestoreHost = process.env.FIRESTORE_EMULATOR_HOST;
 const projectId = process.env.GCLOUD_PROJECT;
+
+const DEV_DOMAIN = 'activitypub-dev.hakatashi.com';
 
 describe('activitypub', () => {
 	jest.setTimeout(10000);
@@ -72,6 +74,46 @@ describe('activitypub', () => {
 				const response = await request(activitypub).get('/nodeinfo/2.1');
 				expect(response.status).toBe(200);
 				expect(response.body.usage.users.total).toBe(3);
+			});
+		});
+	});
+
+	describe('/.well-known', () => {
+		describe('/.well-known/webfinger', () => {
+			beforeEach(async () => {
+				const actor = await apex.createActor('hakatashi', 'Koki Takahashi', '', '', 'Person');
+				await apex.store.saveObject(actor);
+			});
+
+			test('Non-existent user', async () => {
+				const response = await request(activitypub).get(`/.well-known/webfinger?resource=acct:notfound@${DEV_DOMAIN}`);
+				expect(response.status).toBe(404);
+			});
+
+			test('Existing user', async () => {
+				const response = await request(activitypub).get(`/.well-known/webfinger?resource=acct:hakatashi@${DEV_DOMAIN}`);
+				expect(response.status).toBe(200);
+				expect(response.body.subject).toBe(`acct:hakatashi@${DEV_DOMAIN}`);
+				expect(response.body.links).toHaveLength(1);
+				expect(response.body.links[0].rel).toBe('self');
+				expect(response.body.links[0].type).toBe('application/activity+json');
+				expect(response.body.links[0].href).toBe(`https://${DEV_DOMAIN}/activitypub/u/hakatashi`);
+			});
+		});
+
+		describe('/.well-known/nodeinfo', () => {
+			test('Returns nodeinfo', async () => {
+				const response = await request(activitypub).get('/.well-known/nodeinfo');
+				expect(response.status).toBe(200);
+				expect(response.body.links).toHaveLength(2);
+				expect(response.body.links).toContainEqual({
+					rel: 'http://nodeinfo.diaspora.software/ns/schema/2.0',
+					href: `https://${DEV_DOMAIN}/nodeinfo/2.0`,
+				});
+				expect(response.body.links).toContainEqual({
+					rel: 'http://nodeinfo.diaspora.software/ns/schema/2.1',
+					href: `https://${DEV_DOMAIN}/nodeinfo/2.1`,
+				});
 			});
 		});
 	});
