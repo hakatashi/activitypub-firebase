f3a36c4046fd47b886eec1b38835ee5cd329a92b
Koki Takahashi <hakatasiloving@gmail.com>
2023-07-26 15:12:41 +0000

    Add mastodon.spec.ts

---
+++ functions/test/integration/mastodon.spec.ts
@@ -0,0 +1,45 @@
+import {describe, expect, test, afterEach, jest} from '@jest/globals';
+import request from 'supertest';
+import {mastodonApi as mastodon} from '../../src/mastodon/index.js';
+
+const firestoreHost = process.env.FIRESTORE_EMULATOR_HOST;
+const projectId = process.env.GCLOUD_PROJECT;
+
+describe('mastodon', () => {
+	jest.setTimeout(10000);
+
+	// Teardown firestore database after each test
+	afterEach(async () => {
+		await fetch(
+			`http://${firestoreHost}/emulator/v1/projects/${projectId}/databases/(default)/documents`,
+			{
+				method: 'DELETE',
+			},
+		);
+	});
+
+	test('Root path should not be implemented', async () => {
+		const response = await request(mastodon).get('/');
+		expect(response.status).toBe(404);
+	});
+
+	describe('/api', () => {
+		describe('/api/v1/instance', () => {
+			test('Returns instance information', async () => {
+				const response = await request(mastodon).get('/api/v1/instance');
+				expect(response.status).toBe(200);
+				expect(response.body.uri).toBe('hakatashi.com');
+				expect(response.body.title).toBe('HakataFediverse');
+			});
+		});
+
+		describe('/api/v2/instance', () => {
+			test('Returns instance information', async () => {
+				const response = await request(mastodon).get('/api/v2/instance');
+				expect(response.status).toBe(200);
+				expect(response.body.domain).toBe('hakatashi.com');
+				expect(response.body.title).toBe('HakataFediverse');
+			});
+		});
+	});
+});
